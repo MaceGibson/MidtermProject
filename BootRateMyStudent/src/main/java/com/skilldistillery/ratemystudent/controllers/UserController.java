d482f3215e75c6f7ed3ed05b1486f5abd2bfa7bd
MaceGibson <mace_gibson@yahoo.com>
2024-02-05 12:16:32 -0500

    Show student/school details controller impl

--- BootRateMyStudent/src/main/java/com/skilldistillery/ratemystudent/controllers/UserController.java
+++ BootRateMyStudent/src/main/java/com/skilldistillery/ratemystudent/controllers/UserController.java
@@ -15,29 +15,48 @@ import com.skilldistillery.ratemystudent.entities.User;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserDAO userDAO;
-	
-	@GetMapping(path={"/", "home.do"})
+
+	@GetMapping(path = { "/", "home.do" })
 	public String home(Model model) {
 		User u = userDAO.authenticateUser("teacher", "teacher");
 		model.addAttribute("testUser", u);
 		return "home";
 	}
-	
-	@GetMapping(path="searchSchoolResults.do")
+
+	@GetMapping(path = "searchSchoolResults.do")
 	public String searchSchoolRequest(@RequestParam("school") String school, Model model) {
 		List<School> schools = userDAO.searchByschool(school);
 		model.addAttribute("schools", schools);
 		return "searchResults";
 	}
-	
-	@GetMapping(path="searchStudentResults.do")
+
+	@GetMapping(path = "searchStudentResults.do")
 	public String searchStudentRequest(@RequestParam("student") String student, Model model) {
 		List<Student> students = userDAO.searchByStudent(student);
 		model.addAttribute("students", students);
 		return "searchResults";
+	}
+
+	@GetMapping(path = "details.do", params = "id")
+	private String showStudent(@RequestParam("id") int id, Model model) {
+		Student student = userDAO.findByStudentId(id);
+		if (student != null) {
+			model.addAttribute("student", student);
+			return "details";
+		}
+		return "home";
+	}
+	@GetMapping(path = "details.do", params = "id")
+	private String showSchool(@RequestParam("id") int id, Model model) {
+		School school = userDAO.findBySchoolId(id);
+		if (school != null) {
+			model.addAttribute("school", school);
+			return "details";
+		}
+		return "home";
 	}
 
 }
